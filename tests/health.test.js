8394c4136f3478b5ec6c131c51397c42cd346b63
abhishekvenkata1997 <abhishekvenkata.199715@gmail.com>
2023-10-01 20:05:15 -0400

    Make changes to run testing #6

--- tests/health.test.js
+++ tests/health.test.js
@@ -1,7 +1,6 @@
-const app  = require('./app');
+const app  = require('./../app');
 const supertest = require('supertest');
 const request = supertest(app);
-
 
 describe('/healthz', () => {
     it('should return a 200 status code', async () => {
