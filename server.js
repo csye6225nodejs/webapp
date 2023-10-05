23156d85a77151acdd0c588a7ce71281a3ac83d4
abhishekvenkata1997 <abhishekvenkata.199715@gmail.com>
2023-10-04 21:33:40 -0400

    add changes to db

--- server.js
+++ server.js
@@ -3,14 +3,6 @@ const bodyParser = require('body-parser');
 const app = require('./app');
 const port = process.env.PORT;
 
-const { setupDatabase } = require('./services/loadDb.js');
-
-setupDatabase()
-.then(() => {
-
-}).catch((err) => {
-    console.error("error setting up database"+err);
-});
 
 app.listen(port, () => {
     console.log(`Server is now running on port ${port}`);
--- services/dbService.js
+++ services/dbService.js
@@ -4,27 +4,20 @@ const Account = require('./../models/Account');
 
 // Function to start the database connection
 async function startDb() {
-    try {
-        try{
-            await sequelize.authenticate();
-        }  
-        catch (error) {
-            console.log("error in authentication"+error);
-            return false;
-        } 
-        console.log("Connected to the Database");
-        try{
-            await sequelize.sync();
-        } catch (error) {
-            console.log("error in connecting to database");
-            return false;
-        }
-        console.log("Database synchronized");
-        return true;
-    } catch (error) {
-        console.log("Error connecting to db");
+        
+    try{
+        
+        await sequelize.authenticate();
+        console.log("connected to the db");
+        
+        await sequelize.sync();
+        console.log("error in connecting to database");
+         return true;
+     } catch(error)
+     {
+        console.error("error ", error.message);
         return false;
-    }
+     }
 }
 
 module.exports = { startDb };
