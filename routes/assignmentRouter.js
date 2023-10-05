5f7a2b42a3cf41402777eb39ef67ff698f8419b6
abhishekvenkata1997 <abhishekvenkata.199715@gmail.com>
2023-10-04 20:28:08 -0400

    Added scripting and readme

--- routes/assignmentRouter.js
+++ routes/assignmentRouter.js
@@ -11,16 +11,33 @@ router.get("*",(req,res) => {
     res.status(404).send();
 });
 
+
 router.post('/',authMiddleware.basicAuth, assignmentController.addAssignment);
+router.put('/',(req,res) => {
+    res.status(400).send();
+});
 router.put('/:id', authMiddleware.basicAuth, assignmentController.updateAssignment);
+router.delete('/',(req,res) => {
+    res.status(400).send();
+});
 router.delete('/:id', authMiddleware.basicAuth, assignmentController.deleteAssignment);
-router.patch(':/id',res.status(405).send());
+router.patch(':/id',(req,res) => {
+    res.status(405).send();});
 
+router.get("*",(req,res) => {
+        res.status(404).send();});
 
-router.patch('*', res.status(404).send());
-router.post('*',res.status(404).send());
-router.put('*',res.status(404).send());
-router.delete('*',res.send(404).send());
-router.patch('*', res.status(404).send());
+router.patch('*', (req,res) => {
+    res.status(404).send()});
+router.post('*',(req,res) => {
+    res.status(404).send()});
+router.put('*', (req,res) => {
+    res.status(404).send()});
+router.put('*',(req,res) => {
+    res.status(404).send()});
+router.delete('*',(req,res) => {
+    res.status(404).send()});
+router.patch('*', (req,res) => {
+    res.status(404).send()});
 
 module.exports = router;
