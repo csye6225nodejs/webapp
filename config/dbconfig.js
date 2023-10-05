4e1d692b0f5f58d6eec9bad7adcde698de9c6d33
abhishekvenkata1997 <abhishekvenkata.199715@gmail.com>
2023-10-04 21:47:22 -0400

    Deleting loaddb.js

--- config/dbconfig.js
+++ config/dbconfig.js
@@ -1,7 +1,7 @@
 const Sequelize = require('sequelize');
 
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-    host: process.env.DB_HOST,
+const sequelize = new Sequelize("cloudschema", "root", "Abhi$3534", {
+    host: 'localhost',
     dialect: 'mysql',
 });
 
