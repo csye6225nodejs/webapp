0e12e0c3351cca3aaf7a7f20cd3de944a63f8d2e
abhishekvenkata1997 <abhishekvenkata.199715@gmail.com>
2023-10-04 21:18:39 -0400

    Add changes

--- services/loadDb.js
+++ services/loadDb.js
@@ -1,14 +1,14 @@
-const mysql = require('mysql2/promise');
+const mysql = require('mysql2');
 
 const setupDatabase = async () => {
 
-    const connection = await mysql.createConnection({
+    const connection = mysql.createConnection({
     host:'localhost', //localhost
     user: 'root',     // Root user
     password: 'Abhi$3534' // Set your desired root password
   });
 
-    await connection.query('CREATE DATABASE IF NOT EXISTS cloudschema');
+    connection.query('CREATE DATABASE IF NOT EXISTS cloudschema');
     //const username = process.env.DB_HOST; // Replace with your desired username
    //const password = process.env.DB_PASSWORD; // Replace with your desired password
   console.log('MySQL setup completed.');
