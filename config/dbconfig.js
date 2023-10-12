9657048c8872ce6c8e6455ad13ed34a1070e0249
abhishekvenkata1997 <abhishekvenkata.199715@gmail.com>
2023-10-12 01:44:01 -0400

    add readme and change config

--- config/dbconfig.js
+++ config/dbconfig.js
@@ -1,7 +1,7 @@
 const Sequelize = require('sequelize');
 
 const sequelize = new Sequelize("cloudschema", "root", "Abhi$3534", {
-    host: 'localhost',
+    host: '127.0.0.1',
     dialect: 'mysql',
 });
 
