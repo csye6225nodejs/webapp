e236a3f353ed692a4a53125b994dd6c5d51015cd
abhishekvenkata1997 <abhishekvenkata.199715@gmail.com>
2023-10-17 19:10:42 -0400

    test on merge packer build

--- install-app-dependencies.sh
+++ install-app-dependencies.sh
@@ -1,15 +1,26 @@
 #!/bin/bash
 
+
+sudo apt update
+sudo apt install unzip
+
+#ZIP_FILE=/home/admin/webapp.zip
+#TARGET_DIR=/home/admin
+#TARGET_DIRup=/home/admin/webapp
+#unzip /home/admin/webapp.zip -d /home/admin
+
 # Install NVM
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | bash
 
 # Source NVM (you don't need to provide 'nvm' as an argument)
 source ~/.nvm/nvm.sh
 
+
+#cd $TARGET_DIRup || exit
 # Install Node.js version 20.7.0 (assuming it's a valid version)
 nvm install 20.7.0
 nvm use 20.7.0
-
+    
 # Update package lists
 sudo apt update
 
@@ -17,10 +28,8 @@ sudo apt update
 sudo apt install -y mariadb-server
 sudo apt install -y npm
 
-# Change directory to your application's location
-npm init
 # Install application dependencies using npm
-npm install
+#npm install
 
 # Start MariaDB service
 sudo systemctl start mariadb
@@ -28,4 +37,3 @@ sudo systemctl enable mariadb
 
 
 # Start your application (you should use the appropriate command for your application)
-
