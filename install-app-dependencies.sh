b26f92e8dbb541cf2d74a1b92e0b1a68d02a152c
abhishekvenkata1997 <abhishekvenkata.199715@gmail.com>
2023-10-18 03:01:34 -0400

    add changes

--- install-app-dependencies.sh
+++ install-app-dependencies.sh
@@ -4,10 +4,11 @@
 sudo apt update
 sudo apt install unzip
 
-#ZIP_FILE=/home/admin/webapp.zip
-#TARGET_DIR=/home/admin
-#TARGET_DIRup=/home/admin/webapp
-#unzip /home/admin/webapp.zip -d /home/admin
+cd /home/admin || exit
+mkdir webapp
+unzip /home/admin/webapp.zip -d /home/admin/webapp
+
+cd /home/admin/webapp || exit
 
 # Install NVM
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | bash
@@ -15,8 +16,6 @@ curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | bash
 # Source NVM (you don't need to provide 'nvm' as an argument)
 source ~/.nvm/nvm.sh
 
-
-#cd $TARGET_DIRup || exit
 # Install Node.js version 20.7.0 (assuming it's a valid version)
 nvm install 20.7.0
 nvm use 20.7.0
@@ -35,5 +34,11 @@ sudo apt install -y npm
 sudo systemctl start mariadb
 sudo systemctl enable mariadb
 
+service mysql start
+mysql -u root <<MYSQL_SCRIPT
+CREATE DATABASE IF NOT EXISTS cloudschema;
+ALTER USER 'root'@'localhost' IDENTIFIED BY 'Abhi\$3534'
+
+MYSQL_SCRIPT
 
 # Start your application (you should use the appropriate command for your application)
