6596782dd6870cc7a526b7d83cafb78232e51b79
abhishekvenkata1997 <abhishekvenkata.199715@gmail.com>
2023-10-23 22:22:08 -0400

    add changes to test assignment 6

--- config/dbconfig.js
+++ config/dbconfig.js
@@ -1,7 +1,7 @@
 const Sequelize = require('sequelize');
 
-const sequelize = new Sequelize("cloudschema", "root", "Abhi$3534", {
-    host: '127.0.0.1',
+const sequelize = new Sequelize("csye6225", "csye6225", "Abhi$3534", {
+    host: 'csye622536c4a87.crjebeyojvne.us-east-2.rds.amazonaws.com',
     dialect: 'mysql',
 });
 
