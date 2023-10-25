bfc964850ba3a8ed729a3cd2a196207688736564
abhishekvenkata1997 <abhishekvenkata.199715@gmail.com>
2023-10-25 05:14:37 -0400

    add changes

--- config/dbconfig.js
+++ config/dbconfig.js
@@ -1,9 +1,10 @@
 const Sequelize = require('sequelize');
 
-const DB_NAME = "cloudschema";
-const DB_USER = "root";
-const DB_PASSWORD = "Abhi$3534";
-const DB_HOST = "127.0.0.1";
+const DB_NAME = process.env.DB_NAME || "cloudschema";
+const DB_USER = process.env.DB_USER || "root";
+const DB_PASSWORD = process.env.DB_PASSWORD || "Abhi$3534";
+const DB_HOST = process.env.DB_HOST || "127.0.0.1";
+
 
 const sequelize = new Sequelize(DB_NAME, DB_USER, DB_PASSWORD, {
     host: DB_HOST,
