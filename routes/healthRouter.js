7b3dcddf551a5a24577c779c7832fd82e89b0ad0
abhishekvenkata1997 <abhishekvenkata.199715@gmail.com>
2023-11-05 17:06:15 -0500

    add changes

--- routes/healthRouter.js
+++ routes/healthRouter.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const fs = require("fs");
+const {app, logger} = require('./../app');
 const router = express.Router();
 const { healthcontroller } = require('../controllers/healthcontroller');
 
@@ -6,32 +8,41 @@ const { healthcontroller } = require('../controllers/healthcontroller');
 router.get('/', healthcontroller);
 
 router.get('*', (req, res) => {
+  logger.info("unknown api get request gives 404 error");
   res.status(404).send();
 });
 
 router.put('/', (req, res) => {
+    logger.info("/ path put request gives 405 error");
     res.status(405).send();
   });
 
 router.post('/',(req,res) => {
+  logger.info("/ path post request gives 405 error");
   res.status(405).send();
 });
 router.post('*', (req,res) => {
+  logger.info("unknown post request gives 404 error");
   res.status(404).send();
 });
 router.put('*',(req,res) => {
+  logger.info("unknown put request gives 404 error");
   res.status(404).send();
 });
 router.delete('/', (req, res) => {
+    logger.info("/ delete request gives 405 error");
     res.status(405).send();
 });
 router.delete('*',(req,res) => {
+  logger.info("unknown delete request gives 405 error");
   res.status(404).send();
 });
 router.patch('/', (req, res) => {
+  logger.info("/ patch request gives 405 error");
   res.status(405).send();
 });
 router.patch('*',(req,res) => {
+  logger.info("unknown patch request gives 405 error");
   res.status(405).send();
 });
 
